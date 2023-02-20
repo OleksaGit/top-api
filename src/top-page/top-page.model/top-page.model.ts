66c472da1453847278a0fd5cecba319828261e0f
Alex <alexandr.pogre@gmail.com>
2023-02-20 22:49:30 +0200

    add typegoose models

--- src/top-page/top-page.model/top-page.model.ts
+++ src/top-page/top-page.model/top-page.model.ts
@@ -1,26 +1,64 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
 export enum TopLevelCategory {
 	Courses,
 	Services,
 	Books,
 	Products,
 }
-export class TopPageModel {
-	_id: string;
+
+export class HhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSalary: number;
+
+	@prop()
+	middleSalary: number;
+
+	@prop()
+	seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+	@prop()
+	tittle: string;
+
+	@prop()
+	descriptions: string;
+}
+
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopLevelCategory })
 	firstLevelCategory: TopLevelCategory;
+
+	@prop()
 	secondCategory: string;
+
+	@prop({ unique: true })
+	alias: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	category: string;
-	hh?: {
-		count: number;
-		juniorSalary: number;
-		middleSalary: number;
-		seniorSalary: number;
-	};
-	advantages: {
-		tittle: string;
-		descriptions: string;
-	} [];
+
+	@prop({ type: () => HhData})
+	hh?: HhData
+
+	@prop({ type: () => [TopPageAdvantage]})
+	advantages: TopPageAdvantage [];
+
+	@prop()
 	seoText: string;
+
+	@prop()
 	tagsTitle: string;
+
+	@prop({ type: () => [String]})
 	tags: string[];
 }
