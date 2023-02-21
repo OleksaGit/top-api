d807eec7030204015786077b7f2942b00869db17
Alex <alexandr.pogre@gmail.com>
2023-02-21 19:06:12 +0200

    add e2e test review module

---
+++ test/review.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { CreateReviewDto } from '../src/review/dto/create-review.dto';
+import { Types, disconnect } from 'mongoose';
+
+const productId = new Types.ObjectId().toHexString()
+
+const testDto: CreateReviewDto = {
+	name: 'Test',
+	title: 'Title',
+	description: 'Descriptions',
+	rating: 5,
+	productId,
+}
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let createdId: string;
+
+  beforeEach(async () => {
+	const moduleFixture: TestingModule = await Test.createTestingModule({
+		imports: [AppModule],
+	}).compile();
+
+	app = moduleFixture.createNestApplication();
+	await app.init();
+  });
+
+	it('/review/create (POST)', async () => {
+		return request(app.getHttpServer())
+			.post('/review/create')
+			.send(testDto)
+			.expect(201)
+			.expect(({ body }: request.Response) => {
+				createdId = body._id;
+				expect(createdId).toBeDefined()
+			})
+
+			// .then(({ body }: request.Response) => {
+			// 	createdId = body._id;
+			// 	expect(createdId).toBeDefined()
+			// 	done()
+			// })
+	});
+
+	it('/review/byProduct/:productId (GET)', async () => {
+		return request(app.getHttpServer())
+			.get('/review/byProduct/' + productId)
+			.send(testDto)
+			.expect(200)
+			.expect(({ body }: request.Response) => {
+				expect(body.length).toBe(1)
+			})
+	});
+
+	it('/review/:id (DELETE)', () => {
+		return request(app.getHttpServer())
+			.delete('/review/' + createdId)
+			.send(testDto)
+			.expect(200)
+	});
+
+	afterAll(() => {
+		disconnect()
+	})
+
+
+	/**default test
+	 *
+	 */
+  // it('/ (GET)', () => {
+	// return request(app.getHttpServer())
+	// 	.get('/')
+	// 	.expect(200)
+	// 	.expect('Hello World!');
+  // });
+});
