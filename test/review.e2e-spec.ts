569c9819c0ceb159a4daa6a46c682bb292450e59
Alex <alexandr.pogre@gmail.com>
2023-02-24 00:00:00 +0200

    add class-validator and class-transformer

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -28,7 +28,7 @@ describe('AppController (e2e)', () => {
 	await app.init();
   });
 
-	it('/review/create (POST)', async () => {
+	it('/review/create (POST) success', async () => {
 		return request(app.getHttpServer())
 			.post('/review/create')
 			.send(testDto)
@@ -43,6 +43,13 @@ describe('AppController (e2e)', () => {
 			// 	expect(createdId).toBeDefined()
 			// 	done()
 			// })
+	});
+
+	it('/review/create (POST) fail', () => {
+		return request(app.getHttpServer())
+			.post('/review/create')
+			.send({...testDto, rating: 0 })
+			.expect(400)
 	});
 
 	it('/review/byProduct/:productId (GET)', async () => {
