2a5a4a6e9a39dc0eadae05118ce97a276b6d6eea
Alex <alexandr.pogre@gmail.com>
2023-02-24 15:54:00 +0200

    implement login  user + create JWT token

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'Такий користувач вже зареєстрований'
+export const USER_NOT_FOUND_ERROR = 'Користувача з таким email не знайдено'
+export const WRONG_PASSWORD_ERROR = 'Невірний пароль'
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { UserModel } from './auth.model/user.model';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from '../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
@@ -14,7 +17,12 @@ import { AuthService } from './auth.service';
             collection: 'User'
           }
         }
-      ])
+      ]),
+      JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: getJWTConfig,
+      })
 
   ],
   providers: [AuthService]
