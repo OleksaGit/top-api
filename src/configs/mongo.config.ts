093b444c45f3da2358f067e4cfab4a2176c49986
Alex <alexandr.pogre@gmail.com>
2023-03-09 10:28:42 +0200

    add new instance mongoDB

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -2,11 +2,26 @@ import { ConfigService } from '@nestjs/config';
 import { TypegooseModuleOptions } from 'nestjs-typegoose';
 
 export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
-	return {
-		uri: getMongoString(configService),
-		//...getMongoOptions(),
+	const asService = configService.get('MONGO_AS_SERVICE')
+
+	if (asService === 'true') {
+		return {
+			uri: getMongoStringAsService(configService)
+		}
 	}
+	if (asService === 'false') {
+		return {
+			uri: getMongoString(configService)
+		}
+	}
+
+	// return {
+	// 	uri: getMongoString(configService),
+	// 	//...getMongoOptions(),
+	// }
 }
+
+const getMongoStringAsService = (configService: ConfigService) => configService.get('MONGO_URI_AS_SERVICE')
 
 const getMongoString = (configService: ConfigService) =>
 	'mongodb://' +
