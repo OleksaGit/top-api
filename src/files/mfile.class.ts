882fcf3200153b390b06211f1834058929e25380
Alex <alexandr.pogre@gmail.com>
2023-03-15 18:25:17 +0200

    add converts files

---
+++ src/files/mfile.class.ts
@@ -0,0 +1,11 @@
+import * as Buffer from 'buffer';
+
+export class MFile {
+	originalname: string;
+	buffer: Buffer;
+
+	constructor(file: Express.Multer.File | MFile) {
+		this.originalname = file.originalname;
+		this.buffer = file.buffer
+	}
+}
